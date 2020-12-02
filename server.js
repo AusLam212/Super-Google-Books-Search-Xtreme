5279db3480d5c055cf121d89401ebe4f80e1510f
Austin Lambert <atlambert11@gmail.com>
2020-12-01 22:59:30 -0600

    Possible server.js fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const path = require('path');
+
 
 const PORT = process.env.PORT || 3001;
 
@@ -14,9 +14,6 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
-  app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-});
 }
 // Add routes, both API and view
 app.use(routes);
